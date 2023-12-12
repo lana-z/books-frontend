4aa753f445fb524a1a91b3414f9c30070f82d086
lana-z <lana@levelupeconomy.com>
2023-12-11 19:27:58 -0800

    fetching books

--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+const local = import.meta.env.VITE_LOCAL;
 
-class BestBooks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: []
-    }
-  }
+
+// class BestBooks extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       books: []
+//     }
+//   }
+
+
+// componentDidMount(){
+//   this.fetchBooks();
+// }
+
+// async fetchBooks();
+//   try {
+//     const response = await axios.get(`${local}`)
+//   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
-  render() {
+function BestBooks(){
+
+const [books, setBooks] = useState('');
+
+async function fetchBooks() {
+  try {
+    let responseBook = await axios.get(`${local}/books`);
+    setBooks(responseBook.data);
+  } catch {
+    console.error('Books server call not working.');
+  }
+
+  
+
+}
+
+useEffect(() => {
+  fetchBooks();
+
+}, [])
+console.log(books);
+  // render() {
 
     /* TODO: render all the books in a Carousel */
 
@@ -18,14 +53,15 @@ class BestBooks extends React.Component {
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
+        {/* {this.state.books.length ? (
           <p>Book Carousel coming soon</p>
         ) : (
           <h3>No Books Found :(</h3>
-        )}
+        )} */}
       </>
     )
-  }
+  // }
+
 }
 
 export default BestBooks;
