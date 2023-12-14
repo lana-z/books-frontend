1d29203877464107cc7205fbd82d372722ae4ad7
lana-z <lana@levelupeconomy.com>
2023-12-13 16:29:45 -0800

    bootstrap button added

--- src/UpdateBook.jsx
+++ src/UpdateBook.jsx
@@ -1,5 +1,6 @@
 
 import React, {useState, useEffect} from 'react';
+import Button from 'react-bootstrap/Button';
 
 function UpdateBook(props) {
 
@@ -22,11 +23,17 @@ function UpdateBook(props) {
     }, [props.book])
 
     return (
+        <>
+        <div>
+            If you'd like to update a book, click on the book title or description.            
+        </div>
         <form onSubmit={handleSubmit}>
             <input onChange={handleChange} name="title" value={book.title}/>
             <input onChange={handleChange} name="description" value={book.description}/>
-            <button type="submit">Update Book</button>
+            {/* <button type="submit">Update Book</button> */}
+            <Button variant="outline-secondary" as="input" type="submit" value="Update Book"></Button>{' '}
         </form>
+        </>
     )
 }
 
